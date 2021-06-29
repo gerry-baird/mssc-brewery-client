41d542dfbf2cd7897a38c303bbd012a896ffa508
gerry <gerry.baird@uk.ibm.com>
2021-06-29 14:21:06 +0100

    HTTp Get with Spring RestTemplate

--- src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -1,13 +1,18 @@
 package guru.springframework.msscbreweryclient;
 
+import guru.springframework.msscbreweryclient.web.client.BreweryClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MsscBreweryClientApplicationTests {
 
+    @Autowired
+    BreweryClient client;
+
     @Test
-    public void contextLoads() {
+    void getBeerId() {
     }
 
 }
