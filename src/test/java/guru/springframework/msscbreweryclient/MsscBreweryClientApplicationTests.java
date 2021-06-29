346c00e342708ca8d237c2265953951c2224178d
gerry <gerry.baird@uk.ibm.com>
2021-06-29 14:26:36 +0100

    HTTP Post

--- src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -1,18 +1,13 @@
 package guru.springframework.msscbreweryclient;
 
-import guru.springframework.msscbreweryclient.web.client.BreweryClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MsscBreweryClientApplicationTests {
 
-    @Autowired
-    BreweryClient client;
-
     @Test
-    void getBeerId() {
+    public void contextLoads() {
     }
 
 }
